f6d04e40c4bcc8129a74331f43f441a9234e3e1f
Gokul200318 <165988049+Gokul200318@users.noreply.github.com>
2025-02-15 11:53:26 +0530

    Constructor Overloading

---
+++ Book.java
@@ -0,0 +1,24 @@
+// Constructor Overloading
+// Write a Java program to create a class called "Book" with instance variables title, author, and price. Implement a default constructor and two parameterized constructors:
+
+public class Book {
+
+    String title;
+    String author;
+    int price;
+
+    public Book(String title, String author){
+
+        this.title = title;
+        this.author = author;
+    }
+    public Book(String title, String author, int price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+    public static void main(String[] args) {
+        Book mybook = new Book("habits", "gokul",300);
+        System.out.println(mybook.title + ":" + mybook.author +":" + mybook.price);
+    }
+}
