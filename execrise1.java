0b6fcd208f4bbf8278063d7535afaca449abddeb
Gokul200318 <165988049+Gokul200318@users.noreply.github.com>
2025-02-15 10:17:37 +0530

    Create a constructor

---
+++ execrise1.java
@@ -0,0 +1,14 @@
+public class execrise1{
+
+    int age;
+    String name;
+
+    public execrise1(){
+        age = 20;
+        name = "Gokul";
+    }
+    public static void main(String[] args) {
+        execrise1 obj = new execrise1();
+        System.out.println(obj.age + " : "+ obj.name);
+    }
+}
